43b65f2d2ff5a0a08b0c1ebfcd001780c949a963
Madoha <madoha00@gmail.com>
2024-02-19 00:07:15 +0600

    update

--- tablestz_api/tablestz_api/obj/Debug/net6.0/tablestz_api.AssemblyInfo.cs
+++ tablestz_api/tablestz_api/obj/Debug/net6.0/tablestz_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tablestz_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d484a8ed385f478be7670cc495b50021574262")]
 [assembly: System.Reflection.AssemblyProductAttribute("tablestz_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tablestz_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
